ac4e3ca7f23e5b69b0cf2f845bbb447de589ca14
Prashanth <prashanth@gmail.com>
2026-07-13 10:56:36 +0530

    initial commit

---
+++ VariablesDataTypes/Overview.py
@@ -0,0 +1,4 @@
+#1. Python is dynamically Typed programmming Language.
+#2. In Python While Declearing Variabels, We not need to mention Data Types.
+#3. Data Types Will Be assinged by language based on value store into variables.
+#4. 
